6b31ffa3c3a80b577392f42ef9f3ed15b331d275
ijbd <ijbd@umich.edu>
2021-02-15 23:12:24 -0700

    Init.

---
+++ data/easiur_msc/processEasiurFiles.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import numpy as np 
+import sys, os
+
+PROGRAM_PATH = os.path.dirname(__file__)
+EIA860_FOLDER = os.path.join(PROGRAM_PATH,'../eia8602019')
+EIA860_PLANT_FILENAME = '2___Plant_Y2019.xlsx'
+EASIUR_FILE = 'easiur_plants.csv'
+OUTPUT_FILE = 'msc_by_plant.csv'
+
+df = pd.read_excel(os.path.join(EIA860_FOLDER,EIA860_PLANT_FILENAME), skiprows=1, usecols=['Plant Code','Latitude','Longitude'])
+
+# Drop plants with no location
+df.drop(df.index[df['Latitude'] == ' '], inplace=True)
+
+# numpify
+plantCodes = df['Plant Code'].values
+lats = df['Latitude'].values
+lons = df['Longitude'].values
+
+# fix problem child
+lats[plantCodes==62262] = 42.5246072
+
+# processed dataframe
+processed = pd.read_csv(os.path.join(PROGRAM_PATH,EASIUR_FILE))
+processed['Plant Code'] = plantCodes
+
+# Check
+for i in range(len(plantCodes)):
+    assert(int(lats[i]) == int(processed.at[i,'Latitude']))
+    assert(int(lons[i]) == int(processed.at[i,'Longitude']))
+
+
+# Remove plants with no data
+processed.drop(processed.index[pd.isna(processed['PM25 Annual Ground'])],inplace=True)
+
+# Reorder
+cols = processed.columns.tolist()
+cols.remove('Plant Code')
+cols.insert(0,'Plant Code')
+processed = processed[cols]
+
+# Save to file
+processed.to_csv(os.path.join(PROGRAM_PATH,OUTPUT_FILE))
---
+++ easiur.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd 
+import os, sys 
+
+MODULE_PATH = os.path.dirname(__file__)
+
+def getEasiur(plantCodes):
+    pass
+
+def getEmissions(unitCodes):
+    pass
+
+def getMarginalCosts(unitCodes, plantCodes):
+    pass 
